02975b9ee0ce7a8abbc7a135ce37ac13fb392181
Renan Santos <eng.srenan@gmail.com>
2023-03-16 13:51:50 -0300

    Testes com Cypress

---
+++ cypress.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  projectId: 'x6i1oe',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+};
---
+++ cypress/e2e/1-getting-started/busca.spec.js
@@ -0,0 +1,7 @@
+describe('busca cursos', () =>{
+
+    beforeEach(() => {
+        
+    })
+
+} )
